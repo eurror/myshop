2657ac3164d05eeb05ecaa7db7b0677533454e1f
eurus <edgarpo0401@gmail.com>
2021-05-22 15:23:49 +0600

    added payment

--- orders/views.py
+++ orders/views.py
@@ -18,10 +18,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          context = {'order': order,
-                                     })
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
